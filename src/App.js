a1564212808f74ca12c14a47a83a85e66be3ee5e
ArtemCaster <97943938+ArtemCaster@users.noreply.github.com>
2022-07-26 14:56:07 +0300

    test fullpage.js scroll

--- src/App.js
+++ src/App.js
@@ -1,36 +1,45 @@
 import React from "react";
 import Page from "./components/Page";
-import Fullpage, {
-  FullPageSections,
-  FullpageSection,
-  FullpageNavigation,
-} from "@ap.cx/react-fullpage";
+import fullpage from "fullpage.js/dist/fullpage";
 
 function App() {
+  React.useEffect(
+    () =>
+      new fullpage("#fullpage", {
+        licenseKey: "FULL_PAGE_SCROLL",
+        autoScrolling: true,
+        lockAnchors: false,
+        anchors: ["section1", "section2", "section3", "section4", "section5", "section6"],
+      }),
+    []
+  );
 
   return (
-    <div className="App">
-      <Fullpage>
-        <FullpageNavigation />
-        <FullPageSections>
-          <FullpageSection>
-            <Page title={"section 1"} />
-          </FullpageSection>
-          <FullpageSection>
-            <Page title={"section 2"} />
-          </FullpageSection>
-          <FullpageSection>
-            <Page title={"section 3"} />
-          </FullpageSection>
-          <FullpageSection>
-            <Page title={"section 4"} />
-          </FullpageSection>
-        </FullPageSections>
-      </Fullpage>
-      {/* <button className="scrollToTop" onClick={() => scrollToTop()}>
-        scroll top
-      </button> */}
-    </div>
+    <>
+      <div className="App" id="fullpage">
+        <div className="section">
+          <Page title={"section 1"} />
+        </div>
+        <div className="section">
+          <Page title={"section 2"} />
+        </div>
+        <div className="section">
+          <Page title={"section 3"} />
+        </div>
+        <div className="section">
+          <Page title={"section 4"} />
+        </div>
+        <div className="section">
+          <Page title={"section 5"} />
+        </div>
+        <div className="section">
+          <Page title={"section 6"} />
+        </div>
+      </div>
+      <a className="to-top-button" href="#section1">
+        Наверх
+      </a>
+    </>
   );
 }
 
