64a77613c423e1142a19dd9ae7d2173f356442b5
ArtemCaster <97943938+ArtemCaster@users.noreply.github.com>
2022-12-07 18:54:07 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,170 +1,66 @@
 import "animate.css";
-import Iframe from "react-iframe";
-import { TikTokEmbed } from "react-social-media-embed";
+import { TikTokEmbed, YouTubeEmbed } from "react-social-media-embed";
 
 function App() {
-  // console.log();
-  // const apiKey = "AIzaSyDp5V3Q7PIA7d0rLQc6me58X280lyVrfTk";
-  // const chanelId = "@NIGHTRIDEEE";
-  // // const fetchUrl = `https://www.googleapis.com/youtube/v3/videos?key=[]`
 
-  // fetch(`https://www.tiktok.com/@scout2015/video/6718335390845095173`)
-  //   .then((response) => response.json())
-  //   .then((data) => console.log(data));
+
+  const items = [
+    {youtubevideoLink : "https://www.youtube.com/watch?v=Au2GSKZPcBM", origin : "youtube"},
+    {vkVideoLink : "https://vk.com/video_ext.php?oid=-184347033&id=456246465&hash=2f8be4a1ac719bab&hd=2", origin : "vk"},
+    {tikTokVideoLink : "https://www.tiktok.com/@epicgardening/video/7055411162212633903", origin : "tiktok"}
+  ]
+
+  const youtubevideoLink = "https://www.youtube.com/watch?v=Au2GSKZPcBM";
+  const vkVideoLink = "https://vk.com/video_ext.php?oid=-184347033&id=456246465&hash=2f8be4a1ac719bab&hd=2";
+  const tikTokVideoLink = "https://www.tiktok.com/@epicgardening/video/7055411162212633903";
+
 
   return (
     <main className="wrapper">
       <div className="item">
-        <Iframe
-          url="https://www.youtube.com/embed/pa2J3liXvuA"
-          width="280px"
-          height="200px"
-          id=""
-          className=""
-          display="block"
-          position="relative"
-        />
-        <p className="title">
-          Название видео
-        </p>
-      </div>
-      <div className="item--fullsize">
-        <TikTokEmbed
-          url="https://www.tiktok.com/@epicgardening/video/7055411162212633903"
-          width={280}
-          height={570}
-        />
-        <p className="title">
-          Название видео
-        </p>
+        <img
+          className="image"
+          src="https://i.ytimg.com/vi/pa2J3liXvuA/maxresdefault.jpg"
+          alt="youtubevideo"
+        ></img>
+        <p className="title">Фото из ссылки ютуб</p>
       </div>
       <div className="item">
-        <iframe
-          title={'Video'}
-          src={"https://vk.com/video_ext.php?oid=-84440824&id=456257679&hash=915c6c9d14b4bdc1&hd=2"}
-          width="280px"
-          height="200px"
-          allow="autoplay; encrypted-media; fullscreen; picture-in-picture;"
-          frameBorder="0"
-          allowFullScreen
-        />
-        <p className="title">
-          Название видео
-        </p>
-      </div>
-      <div className="item">
-        <TikTokEmbed
-          url="https://www.tiktok.com/@epicgardening/video/7055411162212633903"
-          width={280}
+        <YouTubeEmbed
           height={200}
-        />
-        <p className="title">
-          Название видео
-        </p>
-      </div>
-      <div className="item--fullsize">
-        <TikTokEmbed
-          url="https://www.tiktok.com/@epicgardening/video/7055411162212633903"
           width={280}
-          height={570}
+          // placeholderImageUrl="https://i.ytimg.com/vi/pa2J3liXvuA/maxresdefault.jpg?t=1670421305388"
+          url={youtubevideoLink}
         />
-        <p className="title">
-          Название видео
-        </p>
-      </div>
-      <div className="item">
-        <Iframe
-          url="https://www.youtube.com/embed/pa2J3liXvuA"
-          width="280px"
-          height="200px"
-          id=""
-          className=""
-          display="block"
-          position="relative"
-        />
-        <p className="title">
-          Название видео
-        </p>
-      </div>
-      <div className="item">
-        <Iframe
-          url="https://www.youtube.com/embed/pa2J3liXvuA"
-          width="280px"
-          height="200px"
-          id=""
-          className=""
-          display="block"
-          position="relative"
-        />
-        <p className="title">
-          Название видео
-        </p>
+        <p className="title">Видео из ютуб</p>
       </div>
       <div className="item">
         <iframe
-          title={'Video'}
-          src="https://vk.com/video_ext.php?oid=-84440824&id=456257679&hash=915c6c9d14b4bdc1&hd=2"
-          width="280px"
-          height="200px"
-          allow="encrypted-media; fullscreen; picture-in-picture;"
+          title={"sdsdsd"}
+          src={vkVideoLink}
+          width="280"
+          height="200"
+          allow="autoplay; encrypted-media; fullscreen; picture-in-picture;"
           frameborder="0"
           allowfullscreen
         ></iframe>
-        <p className="title">
-          Название видео
-        </p>
+        <p className="title">Видео из vk с хэшем</p>
       </div>
       <div className="item">
-        <TikTokEmbed
-          url="https://www.tiktok.com/@epicgardening/video/7055411162212633903"
-          width={280}
-          height={200}
-        />
-        <p className="title">
-          Название видео
-        </p>
+        <iframe
+          title={"sdsdsd"}
+          src={"https://vk.com/video?z=video-71226649_456239441%2Fpl_cat_trends"}
+          width="280"
+          height="200"
+          allow="autoplay; encrypted-media; fullscreen; picture-in-picture;"
+          frameborder="0"
+          allowfullscreen
+        ></iframe>
+        <p className="title">Видео из вк</p>
       </div>
       <div className="item">
-        <Iframe
-          url="https://www.youtube.com/embed/jfKfPfyJRdk"
-          width="280px"
-          height="200px"
-          id=""
-          className=""
-          display="block"
-          position="relative"
-        />
-        <p className="title">
-          Название видео
-        </p>
-      </div>
-      <div className="item">
-        <Iframe
-          url="https://www.youtube.com/embed/0Z8783ZAtmI"
-          width="280px"
-          height="200px"
-          id=""
-          className=""
-          display="block"
-          position="relative"
-        />
-        <p className="title">
-          Название видео
-        </p>
-      </div>
-      <div className="item">
-        <Iframe
-          url="https://www.youtube.com/embed/MkBO8imD57k"
-          width="280px"
-          height="200px"
-          id=""
-          className=""
-          display="block"
-          position="relative"
-        />
-        <p className="title">
-          Название видео
-        </p>
+        <TikTokEmbed url={tikTokVideoLink} width={325} />
+        <p className="title">Видео из тикток</p>
       </div>
     </main>
   );
